ad5c2fcffb0de0ccf1557e0d7d7147598134b932
Pablo <ortegaperaltap@gmail.com>
2025-10-12 22:58:29 -0400

    add signInWithGoogle function to button

--- folio-app/components/LoginWithGoogleButton.tsx
+++ folio-app/components/LoginWithGoogleButton.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import { signInWithGoogle } from "@/lib/auth-actions"
 import { Button } from "./ui/button"
 
 export default function LoginWithGoogleButton() {
   return (
-    <Button variant="outline" type="button">
+    <Button
+      variant="outline"
+      type="button"
+      onClick={() => {
+        signInWithGoogle();
+      }}
+    >
       Login with Google
     </Button>
   )
