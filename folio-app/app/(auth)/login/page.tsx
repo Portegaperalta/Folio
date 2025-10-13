e6b8f2ec24a3690c8fcca30c05e8f8c94c32ecf0
Pablo <ortegaperaltap@gmail.com>
2025-10-12 22:59:01 -0400

    add LoginForm component to login page

--- folio-app/app/(auth)/login/page.tsx
+++ folio-app/app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
 export default function Login() {
   return (
-    <div className="login"></div>
+    <div className="login">
+      <LoginForm />
+    </div>
   )
 }
