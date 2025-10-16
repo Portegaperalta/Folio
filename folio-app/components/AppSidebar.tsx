6df0649e7a2bb0104a936d7a1e98c05997b2158d
Pablo <ortegaperaltap@gmail.com>
2025-10-16 00:05:15 -0400

    add some custom style to dropdown menu

--- folio-app/components/AppSidebar.tsx
+++ folio-app/components/AppSidebar.tsx
@@ -14,38 +14,44 @@ import Link from "next/link"
 
 const items = [
   {
-    title: "Home",
+    title: "All Folders",
     url: "/",
-    icon: <House />,
+    icon: <House color="#F4F4F5" />,
   },
   {
     title: "My Bookmarks",
     url: "/bookmarks",
-    icon: <Folder />,
+    icon: <Folder color="#F4F4F5" />,
   },
   {
     title: "Settings",
     url: "/settings",
-    icon: <Settings />,
+    icon: <Settings color="#F4F4F5" />,
   }
 ]
 
 export default function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>
-            Folio
+      <SidebarContent className="bg-(--clr-bg-ligh-dark)">
+        <SidebarGroup className="space-y-10">
+          <SidebarGroupLabel className="text-(--clr-text-light) text-[1.6rem]">
+            <Link href="/">
+              Folio
+            </Link>
           </SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="space-y-2">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild
+                    className="py-6 hover:bg-(--clr-purple-light) active:bg-(--clr-purple-light)"
+                  >
                     <Link href={item.url}>
                       {item.icon}
-                      <span>{item.title}</span>
+                      <span className="text-(--clr-text-light)">
+                        {item.title}
+                      </span>
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
