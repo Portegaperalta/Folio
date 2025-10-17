dc0610401b2e29c43e31c50e7c0f7b5a79a95773
Pablo <ortegaperaltap@gmail.com>
2025-10-16 22:35:01 -0400

    add and style SidebarFooter with dropdown menu

--- folio-app/components/AppSidebar.tsx
+++ folio-app/components/AppSidebar.tsx
@@ -1,5 +1,19 @@
-import { House, Folder, Settings } from "lucide-react"
+import { House, Folder, Settings, ChevronUp, User2 } from "lucide-react"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import {
   Sidebar,
   SidebarContent,
@@ -9,8 +23,10 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarFooter,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
+import { title } from "process"
 
 const items = [
   {
@@ -33,7 +49,7 @@ const items = [
 export default function AppSidebar() {
   return (
     <Sidebar className="border-r-(--clr-border-dark)">
-      <SidebarContent className="bg-(--clr-bg-ligh-dark)">
+      <SidebarContent className="bg-(--clr-bg-light-dark)">
         <SidebarGroup className="space-y-6 px-0">
           <SidebarGroupLabel className="text-(--clr-purple-light) text-[1.6rem]
           py-6 px-4 border-b-1 rounded-none border-(--clr-border-dark)">
@@ -46,7 +62,8 @@ export default function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild
-                    className="py-6 hover:bg-(--clr-purple-light) active:bg-(--clr-purple-light)"
+                    className="py-6 hover:bg-(--clr-purple-light) 
+                    active:bg-(--clr-purple-light)"
                   >
                     <Link href={item.url}>
                       {item.icon}
@@ -61,6 +78,43 @@ export default function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter className="bg-(--clr-bg-light-dark)">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu >
+              <DropdownMenuTrigger
+                asChild
+                className="cursor-pointer "
+              >
+                <SidebarMenuButton className="text-(--clr-text-light) 
+                hover:text-(--clr-text-light) hover:bg-(--clr-bg-light-dark)">
+                  <User2 />
+                  <p>Pablo E.</p>
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width] 
+                bg-(--clr-bg-light-dark) border-(--clr-border-dark)"
+              >
+                <DropdownMenuItem className="text-(--clr-text-light) 
+                cursor-pointer hover:bg-(--clr-purple-light)">
+                  <Link href="/account">
+                    Account
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-(--clr-text-light)
+                 cursor-pointer hover:bg-(--clr-purple-light)">
+                  <Link href="/logout">
+                    Log out
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
