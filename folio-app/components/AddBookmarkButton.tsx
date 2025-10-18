e25f82f95910f1e67a07ce5f9cba5843dd9a08b4
Pablo <ortegaperaltap@gmail.com>
2025-10-18 00:16:25 -0400

    add title to button for better accessibility

--- folio-app/components/AddBookmarkButton.tsx
+++ folio-app/components/AddBookmarkButton.tsx
@@ -3,7 +3,9 @@ import { Button } from "./ui/button"
 
 export default function AddBookmarkButton() {
   return (
-    <Button className="text-(--clr-text-light) bg-(--clr-purple-light) 
+    <Button
+      title="Add bookmark"
+      className="text-(--clr-text-light) bg-(--clr-purple-light) 
     cursor-pointer hover:bg-(--clr-purple-light)">
       <Plus />
       Add Bookmark
