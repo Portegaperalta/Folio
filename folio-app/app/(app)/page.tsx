6dfd700be687bd414c9402244449c52d21d91ff4
Pablo <ortegaperaltap@gmail.com>
2025-10-18 12:18:47 -0400

    add bookmark count to home-title

--- folio-app/app/(app)/page.tsx
+++ folio-app/app/(app)/page.tsx
@@ -5,9 +5,14 @@ export default function Home() {
     <div className="home pt-4">
       <div className="home-content flex flex-col gap-30">
         <div className="home-top flex flex-row items-center justify-between">
-          <h2 className="text-(--clr-text-light) text-[2rem] font-[500]">
-            All Bookmarks
-          </h2>
+          <div className="home-title">
+            <h2 className="text-(--clr-text-light) text-[2rem] font-[500]">
+              All Bookmarks
+            </h2>
+            <p className="text-(--clr-gray-text)">
+              0 Bookmarks
+            </p>
+          </div>
           <AddBookmarkButton />
         </div>
       </div>
