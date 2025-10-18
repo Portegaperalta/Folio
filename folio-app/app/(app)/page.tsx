30142fa0ed8b52c92d4fed5b19b94e9cf7e59f51
Pablo <ortegaperaltap@gmail.com>
2025-10-17 23:29:15 -0400

    add title to home component

--- folio-app/app/(app)/page.tsx
+++ folio-app/app/(app)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div className="home">
-
+    <div className="home pt-4">
+      <div className="home-content">
+        <div className="home-top">
+          <div className="home-title">
+            <h1 className="text-(--clr-text-light) text-[2rem] font-[500]">
+              All Folders
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
