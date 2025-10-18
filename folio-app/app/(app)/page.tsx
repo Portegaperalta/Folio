496e7c4e3c7ddd18f6a6eedf4763829c1d625c3b
Pablo <ortegaperaltap@gmail.com>
2025-10-18 00:10:46 -0400

    create AddBookmarkButton component (no functionality yet) and add it to home

--- folio-app/app/(app)/page.tsx
+++ folio-app/app/(app)/page.tsx
@@ -1,11 +1,14 @@
+import AddBookmarkButton from "@/components/AddBookmarkButton";
+
 export default function Home() {
   return (
     <div className="home pt-4">
       <div className="home-content flex flex-col gap-30">
-        <div className="all-bookmarks">
+        <div className="home-top flex flex-row items-center justify-between">
           <h2 className="text-(--clr-text-light) text-[2rem] font-[500]">
             All Bookmarks
           </h2>
+          <AddBookmarkButton />
         </div>
       </div>
     </div>
