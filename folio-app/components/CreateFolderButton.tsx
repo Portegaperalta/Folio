d56bb14639856cd2115607cf22912c0d4ac7e8e0
Pablo <ortegaperaltap@gmail.com>
2025-10-22 23:14:34 -0400

    add props to CreateFolderButton and function to update isFolderFormVisible state

--- folio-app/components/CreateFolderButton.tsx
+++ folio-app/components/CreateFolderButton.tsx
@@ -2,14 +2,27 @@
 
 import { Plus } from "lucide-react"
 import { Button } from "./ui/button"
+import { Dispatch, SetStateAction } from "react"
 
-export default function CreateFolderButton() {
+type CreateFolderButtonProps = {
+  setIsFolderFormVisible: Dispatch<SetStateAction<boolean>>
+}
+
+export default function CreateFolderButton({
+  setIsFolderFormVisible
+}: CreateFolderButtonProps) {
+
+  const displayFolderForm = () => {
+    setIsFolderFormVisible(true);
+  }
+
   return (
     <div className="create-new-folder-button">
       <Button
         type="button"
         size="sm"
         title="Create New Folder"
+        onClick={displayFolderForm}
         className="text-(--clr-text-light) bg-transparent rounded-lg
         hover:bg-(--clr-purple-light) cursor-pointer"
       >
