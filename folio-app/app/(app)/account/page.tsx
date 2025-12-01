6761186a08fb17ffec59caf195acbccfcb06acae
Pablo <ortegaperaltap@gmail.com>
2025-11-30 23:06:23 -0400

    add mockup user name and avatar

--- folio-app/app/(app)/account/page.tsx
+++ folio-app/app/(app)/account/page.tsx
@@ -1,11 +1,12 @@
 import { ArrowLeft } from "lucide-react";
+import { User } from "lucide-react";
 import Link from "next/link";
 
 export default function Account() {
   return (
     <div className="pt-4">
-      <div className="content">
-        <div className="content-top">
+      <div className="account-content flex flex-col gap-8">
+        <div className="account-content-top space-y-10">
           <Link
             href="/"
             className="p-2 text-(--clr-text-light) flex flex-row
@@ -14,6 +15,14 @@ export default function Account() {
             <ArrowLeft />
             Back to Home
           </Link>
+          <div className="account-user-avatar flex flex-col gap-4 items-center">
+            <div className="p-4 rounded-full bg-(--clr-purple-dark-bg)">
+              <User color="#9868E5" size={50} />
+            </div>
+            <div className="text-(--clr-text-light) text-[1.4rem] font-[500]">
+              <p>John Doe</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
