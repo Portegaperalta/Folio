bc1c7b4de20ea2be0b60271951d7ec58e0970868
Pablo <ortegaperaltap@gmail.com>
2025-11-30 20:52:39 -0400

    increase form  z-index to 100 to ensure it displays above other elements

--- folio-app/components/CreateFolderForm.tsx
+++ folio-app/components/CreateFolderForm.tsx
@@ -42,7 +42,8 @@ export default function CreateFolderForm({
       onSubmit={handleFormSubmit}
       className={`px-6 py-4 bg-(--clr-bg-light-dark) rounded-lg
       border-1 border-(--clr-border-dark) w-full max-w-120 absolute
-      left-1/2 -translate-x-1/2 translate-y-1/5 ${isFolderFormVisible ? `inline-block` : `hidden`}`}
+      left-1/2 -translate-x-1/2 translate-y-1/5 ${isFolderFormVisible ? `inline-block` : `hidden`}
+      z-100`}
     >
       <FieldSet>
         <div className="form-top flex items-center justify-between">
