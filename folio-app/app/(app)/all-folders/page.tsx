82cddbe45942e2e92c6184ee1ddb2965833dc063
Pablo <ortegaperaltap@gmail.com>
2025-12-02 22:28:34 -0400

    Create All Folders page

---
+++ folio-app/app/(app)/all-folders/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useState } from "react";
+import CreateFolderButton from "@/components/CreateFolderButton";
+import CreateFolderForm from "@/components/CreateFolderForm";
+
+export default function AllFolders() {
+  const [isFolderFormVisible, setIsFolderFormVisible] = useState<boolean>(false);
+
+  return (
+    <div className="all-folders pt-4">
+      <div className="all-folders-content">
+        <div className="all-folders-top flex items-center justify-between">
+          <div className="title">
+            <h2 className="text-(--clr-text-light) text-[1.4rem]
+           font-[500] md:text-[2rem]">
+              All Folders
+            </h2>
+            <p className="text-(--clr-gray-text)">
+              0 Folders
+            </p>
+          </div>
+          <CreateFolderButton setIsFolderFormVisible={setIsFolderFormVisible} />
+        </div>
+        <CreateFolderForm
+          isFolderFormVisible={isFolderFormVisible}
+          setIsFolderFormVisible={setIsFolderFormVisible}
+        />
+        <div className="all-folders-list"></div>
+      </div>
+    </div>
+  )
+}
