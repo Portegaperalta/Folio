dbf966f1e3c93ceed2e785c9949bd93d362e044a
Pablo <ortegaperaltap@gmail.com>
2025-12-09 23:16:59 -0400

    add cursor pointer and background on hover to savechanges button

--- folio-app/app/(app)/account/edit-profile/components/SaveChangesButton.tsx
+++ folio-app/app/(app)/account/edit-profile/components/SaveChangesButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function SaveChangesButton() {
   return (
     <Button className="bg-white text-(--clr-bg-dark) px-6
-     rounded-full">
+     rounded-full cursor-pointer hover:bg-gray-200">
       Save
     </Button>
   )
