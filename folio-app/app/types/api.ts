fa05c74f65d59461d49d65af88fea86cde0df319
Pablo <ortegaperaltap@gmail.com>
2026-02-27 17:50:15 -0400

    Create new types folder and add api DTO types

---
+++ folio-app/app/types/api.ts
@@ -0,0 +1,66 @@
+export type PaginationDTO = {
+  page: number,
+  recordsPerPage: number
+}
+
+export type LoginCredentialsDTO = {
+  email: string,
+  password: string
+}
+
+export type RegisterCredentialsDTO = {
+  name: string,
+  email: string,
+  password: string,
+  phoneNumber?: string
+}
+
+export type FolderDTO = {
+  name: string,
+  isMarkedFavorite: boolean,
+  creationDate: Date,
+  lastVisitedTime?: Date
+}
+
+export type FolderCreationDTO = {
+  name: string
+}
+
+export type FolderUpdateDTO = {
+  name: string,
+  isMarkedFavorite: boolean
+}
+
+export type BookmarkDTO = {
+  id: string,
+  name: string,
+  url: string,
+  isMarkedFavorite: boolean,
+  creationDate: Date,
+  lastVisitedTime?: Date
+}
+
+export type BookmarkCreationDTO = {
+  name: string,
+  url: string,
+}
+
+export type BookmarkUpdateDTO = {
+  id: string,
+  name?: string,
+  url?: string,
+  isMarkedFavorite?: boolean
+}
+
+export type UserProfileDetailsDTO = {
+  name: string,
+  email: string,
+  phoneNumber: string,
+  creationDate?: Date
+}
+
+export type UserUpdateDTO = {
+  name: string,
+  email: string,
+  phoneNumber: string
+}
